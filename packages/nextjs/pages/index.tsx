8e88884bfaea3aef23e9e7704b5c72c9c8997a83
Petr Korolev <sky4winder@gmail.com>
2023-11-18 21:30:37 +0300

    add table

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -10,26 +10,54 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-2xl mb-2">Welcome to </span>
+            <span className="block text-4xl font-bold">Splitwise-Crypto Integration App </span>
           </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <p className="text-center text-lg">Get started by editing </p>
         </div>
+
+        {/* tesi */}
+
+        <div className="container mt-4">
+          <h3>Settle Up</h3>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Recipient</th>
+                <th>Amount</th>
+                <th>Ethereum Address</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>John Doe</td>
+                <td>$50</td>
+                <td>0x123...ABC</td>
+                <td>
+                  <button className="btn btn-success">Settle Up</button>
+                </td>
+              </tr>
+              <tr>
+                <td>Alice</td>
+                <td>$42</td>
+                <td>0xaB3...ABC</td>
+                <td>
+                  <button className="btn btn-success">Settle Up</button>
+                </td>
+              </tr>
+              <tr>
+                <td>Bob</td>
+                <td>$420</td>
+                <td>0x123...def</td>
+                <td>
+                  <button className="btn btn-success">Settle Up</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        {/*input  */}
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
