d989901cb69e045a1e3eb95dba179606e052e612
Petr Korolev <sky4winder@gmail.com>
2023-11-18 21:43:15 +0300

    add metamask integrations

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -13,10 +13,8 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to </span>
             <span className="block text-4xl font-bold">Splitwise-Crypto Integration App </span>
           </h1>
-          <p className="text-center text-lg">Get started by editing </p>
         </div>
 
-        {/* tesi */}
 
         <div className="container mt-4">
           <h3>Settle Up</h3>
@@ -33,25 +31,25 @@ const Home: NextPage = () => {
               <tr>
                 <td>John Doe</td>
                 <td>$50</td>
-                <td>0x123...ABC</td>
+                <td>0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC</td>
                 <td>
-                  <button className="btn btn-success">Settle Up</button>
+                  <button className="btn btn-success1">Settle Up</button>
                 </td>
               </tr>
               <tr>
                 <td>Alice</td>
                 <td>$42</td>
-                <td>0xaB3...ABC</td>
+                <td>0x90F79bf6EB2c4f870365E785982E1f101E93b906</td>
                 <td>
-                  <button className="btn btn-success">Settle Up</button>
+                  <button className="btn btn-success2">Settle Up</button>
                 </td>
               </tr>
               <tr>
                 <td>Bob</td>
                 <td>$420</td>
-                <td>0x123...def</td>
+                <td>0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC</td>
                 <td>
-                  <button className="btn btn-success">Settle Up</button>
+                  <button className="btn btn-success3">Settle Up</button>
                 </td>
               </tr>
             </tbody>
@@ -63,23 +61,10 @@ const Home: NextPage = () => {
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
+
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
             </div>
           </div>
         </div>
